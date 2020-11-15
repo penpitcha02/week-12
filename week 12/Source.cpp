2948086929a675e0d0a47d4c710d47dc60533f1d
PENPITCHA SAPPAKUN <63010709@KMITL.AC.TH>
2020-11-15 21:11:14 +0700

    -

--- week 12/Source.cpp
+++ week 12/Source.cpp
@@ -11,14 +11,14 @@ int main()
 	} s[5];
 	for (i = 0; i < 5; i++)
 	{
-		printf("Student[%d]\n", i);
+		printf("Person[%d]\n", i);
 		printf("\t name:");
 		scanf("%s", s[i].name);
 		printf("\t age:");
 		scanf("%d", &s[i].age);
 	}
 	for (i = 0; i < 5; i++)
-		if (s[i].age > 18)
+		if (s[i].age >= 20)
 			printf("\n %s,%d", s[i].name, s[i].age);
 	return 0;
 }
